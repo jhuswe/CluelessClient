273fde3f46f054f2087bbf6fe2b2da4e94f66dbe
rebchu <rebchu@uw.edu>
2015-12-12 16:16:23 -0500

    Disprove Panel Updated

--- src/panels/DisprovePanel.java
+++ src/panels/DisprovePanel.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.util.*;
 
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
@@ -11,42 +12,31 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import objects.*;
+
 public class DisprovePanel extends JPanel
 {
 	
-	protected JLabel DisproveLabel = new JLabel( "DISPROVE" );	
-	public JButton accusationButton = new JButton( "Make Accusation" );
-	public JButton okay = new JButton( "Okay" );
+	protected JLabel dpLabel = new JLabel( "DISPROVE" );	
+	public JButton disproveButton = new JButton( "Make Disprove" );
+
+	public ArrayList<JCheckBox> checkBox;
 	
-	public List<Card> card
+	public List<Card> playerCards;
 	
 	public DisprovePanel()
 	{
+		super();
 		setLayout( new BorderLayout() );
 		createComponents();
 	}
 	
 	public void createComponents()
 	{
-//		accusationButton.addActionListener( new ActionListener() {
-//
-//			@Override
-//			public void actionPerformed(ActionEvent e) 
-//			{
-//				
-//			}
-//			
-//		});
-		
-		JPanel mainPane = new JPanel( new BorderLayout() );
-		
-		GridBagConstraints gbc = new GridBagConstraints();
-		gbc.anchor = GridBagConstraints.NORTHEAST;
-		
-		JPanel SuspectPane = new JPanel( new GridBagLayout() );
-		JPanel WeaponPane = new JPanel( new GridBagLayout() );
-		
-		for( int i = 0; i < SUSPECTS.length; i++ )
+		JPanel mainPane = new JPanel( new FlowLayout(FlowLayout.CENTER) );
+		disproveButton.setEnabled (false);
+				
+		for( int i = 0; i < playerCards.length; i++ )
 		{
 			JPanel row = new JPanel( new FlowLayout( FlowLayout.RIGHT ) );
 			JLabel lb = new JLabel( SUSPECTS[i] );
@@ -57,19 +47,8 @@ public class DisprovePanel extends JPanel
 			SuspectPane.add( row, gbc );
 		}
 		
-		for( int i = 0; i < WEAPONS.length; i++ )
-		{
-			JPanel row = new JPanel( new FlowLayout( FlowLayout.RIGHT ) );
-			JLabel lb = new JLabel( WEAPONS[i] );
-			JCheckBox box = new JCheckBox();
-			row.add( lb );
-			row.add( box );
-			gbc.gridwidth = GridBagConstraints.REMAINDER;
-			WeaponPane.add( row, gbc );
-		}
 		
 		mainPane.add( SuspectPane, BorderLayout.LINE_START );
-		mainPane.add( WeaponPane, BorderLayout.LINE_END );
 		
 		JPanel buttonPane = new JPanel( new BorderLayout() );
 		buttonPane.add( okay, BorderLayout.LINE_END );
